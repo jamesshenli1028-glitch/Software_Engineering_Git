67bb705342fbf6bb01f0351570c947504e9fa7b5
jamesshenli1028-glitch <james.shenli1028@gmail.com>
2026-01-15 16:02:38 +0100

    update

--- src/Timer_Service/Timer_Service.cpp
+++
@@ -1,6 +0,0 @@
-#include "TimerService.h"
-
-void TimerService::delayMs(int ms)
-{
-    std::this_thread::sleep_for(std::chrono::milliseconds(ms));
-}
--- src/Timer_Service/Timer_Service.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <thread>
-#include <chrono>
-
-class TimerService
-{
-public:
-    static void delayMs(int ms);
-};
